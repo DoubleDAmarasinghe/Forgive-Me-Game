5e6a7732511b05f93098941392396328a2ae3659
Dilanga A.L.D <73058707+DoubleDAmarasinghe@users.noreply.github.com>
2023-02-21 00:04:12 +0530

    Char movements and collectables

---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public TMP_Text scoreText;
+    Blocks testobject;
+    private GameObject test;
+    private int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        testobject = GameObject.FindWithTag("Blocks").GetComponent<Blocks>();
+    }
+
+    public void SetScore()
+    {
+        score += testobject.scorePoint;
+        //if game object attached to this game object
+        //score += gameObject.GetComponent<Blocks>().scorePoint;
+        Debug.Log(score);
+        scoreText.text = score.ToString();
+    }
+}

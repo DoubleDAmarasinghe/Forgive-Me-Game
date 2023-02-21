61b7987df4d6473ecb0642c2c0cb65f9cf384bd0
Dilanga A.L.D <73058707+DoubleDAmarasinghe@users.noreply.github.com>
2023-02-22 00:20:33 +0530

    added jump functionlity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,70 +6,88 @@ public class PlayerController : MonoBehaviour
 {
 
     CharacterController characterController;
+    private Vector3 moveVector;
     public float speed;
-    bool rotated1 = false;
-    bool rotated2 = true;
+
+    public float JumpHeight;
+    private float gravity;
 
     // Start is called before the first frame update
     void Start()
     {
         characterController = gameObject.GetComponent<CharacterController>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        StandingRotation();
+        DirectionalRotation();
+        
 
         float horizontal = Input.GetAxis("Horizontal");
 
         if(this.transform.localEulerAngles.y==0)
         {
-            Vector3 direction = new Vector3(horizontal, 0, 0);
-            characterController.Move(direction*speed);
+            moveVector = new Vector3(horizontal, 0, 0);
+            characterController.Move(moveVector*speed);
         }
 
         else if(this.transform.localEulerAngles.y==180)
         {
-            Vector3 direction = new Vector3(-horizontal, 0, 0);
-            characterController.Move(direction*speed);
+            moveVector = new Vector3(-horizontal, 0, 0);
+            characterController.Move(moveVector*speed);
         }
 
         else if(this.transform.localEulerAngles.y==270)
         {
-            Vector3 direction = new Vector3(0, 0, horizontal);
-            characterController.Move(direction*speed);
+            moveVector = new Vector3(0, 0, horizontal);
+            characterController.Move(moveVector*speed);
         }
 
         else if(this.transform.localEulerAngles.y==90)
         {
-            Vector3 direction = new Vector3(0, 0, -horizontal);
-            characterController.Move(direction*speed);
+            moveVector = new Vector3(0, 0, -horizontal);
+            characterController.Move(moveVector*speed);
         }
+
+
+        gravity += Physics.gravity.y * Time.deltaTime;
+
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            gravity = JumpHeight;
+            
+            Debug.Log("gggggggggggggg");
+        }
+
+        
+        Vector3 v = moveVector * speed;
+        v.y = gravity;
+        characterController.Move(v*Time.deltaTime);
         
         
         Debug.Log(this.transform.localEulerAngles.y);
         
     }
 
-    void StandingRotation()
+    void DirectionalRotation()
     {
-        if(Input.GetKeyUp(KeyCode.Space))
+        //directional rotation to left
+        if(Input.GetKeyUp(KeyCode.O))
         {
-            //Debug.Log("rotated left");
             this.transform.Rotate(0, 90 , 0, Space.World);
-            rotated1 = true;
-            rotated2 = false;
-
         }
 
-        else if(Input.GetKeyUp(KeyCode.Backspace))
+        //directional rotation to right
+        else if(Input.GetKeyUp(KeyCode.P))
         {
-            //Debug.Log("rotated right");
             this.transform.Rotate(0, -90 , 0, Space.World);
-            rotated1 = true;
-            rotated2 = false;
-
         }
+    }
+
+    void Jump()
+    {
+        
     }
 }

2d83b0a69566c37a7448dfdf728c33703b8c0730
Dilanga A.L.D <73058707+DoubleDAmarasinghe@users.noreply.github.com>
2023-02-24 00:14:56 +0530

    correct movemet axis when directional rotate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public GameObject CharacterMesh;
     CharacterController characterController;
     private Vector3 moveVector;
     public float speed;
-
     public float JumpHeight;
     private float gravity;
-
     public float smooth;
+    bool toggle = false;
     private Quaternion targetRotation;
-
-    bool test = false;
 
     // Start is called before the first frame update
     void Start()
@@ -27,54 +24,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        DirectionalRotation();
-
         transform.rotation = Quaternion.Slerp(transform.rotation,targetRotation,10*smooth*Time.deltaTime);
+
+        DirectionalRotation();
+        MovementLogic();
+        Jump();
+
+        //making a custom gravity
+        gravity += Physics.gravity.y * Time.deltaTime;  
+    }
+
+    void MovementLogic()
+    {
         
-        //Debug.Log(this.transform.localEulerAngles.y);
-
         float horizontal = Input.GetAxis("Horizontal");
-
-        if(this.transform.localEulerAngles.y==0)
-        {
-            moveVector = new Vector3(horizontal, 0, 0);
-            characterController.Move(moveVector*speed*Time.deltaTime);
-        }
-
-        else if(this.transform.localEulerAngles.y == 90)
-        {
-            moveVector = new Vector3(-horizontal, 0, 0);
-            characterController.Move(moveVector*speed*Time.deltaTime);
-        }
-
-        else if(this.transform.localEulerAngles.y==270)
+         if(toggle)
         {
             moveVector = new Vector3(0, 0, horizontal);
             characterController.Move(moveVector*speed*Time.deltaTime);
         }
 
-        else if(this.transform.localEulerAngles.y==180)
+        else if(!toggle)
         {
-            moveVector = new Vector3(0, 0, -horizontal);
+            moveVector = new Vector3(horizontal, 0, 0);
             characterController.Move(moveVector*speed*Time.deltaTime);
+            //transform.rotation = Quaternion.AngleAxis(180,Vector3.up);
         }
-
-
-        //jump
-        gravity += Physics.gravity.y * Time.deltaTime;
-
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            gravity = JumpHeight;
-        }
-
-        
-        Vector3 v = moveVector * speed;
-        v.y = gravity;
-        characterController.Move(v*Time.deltaTime);
-        
-        
-        
         
     }
 
@@ -84,13 +59,26 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyUp(KeyCode.O))
         {
             targetRotation *= Quaternion.AngleAxis(90,Vector3.up);
+            toggle=!toggle;
         }
 
         //directional rotation to right
         else if(Input.GetKeyUp(KeyCode.P))
         {
             targetRotation *= Quaternion.AngleAxis(-90,Vector3.up);
-            //this.transform.Rotate(0, -90 , 0, Space.World);
+            toggle=!toggle;
         }
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            gravity = JumpHeight;
+        }
+
+        Vector3 v = moveVector * speed;
+        v.y = gravity;
+        characterController.Move(v*Time.deltaTime);
     }
 }

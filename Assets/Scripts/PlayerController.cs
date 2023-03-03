f4cdaa2fb70710ad91f0f51bb6e9e538b528793d
Dilanga A.L.D <73058707+DoubleDAmarasinghe@users.noreply.github.com>
2023-03-03 22:36:07 +0530

    Directional rotation with smooth and movment logic on correct direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     public float smooth;
     bool toggle = false;
     private Quaternion targetRotation;
+    int compass = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ResetCompass();
+        Debug.Log(compass);
         transform.rotation = Quaternion.Slerp(transform.rotation,targetRotation,10*smooth*Time.deltaTime);
 
         DirectionalRotation();
@@ -38,35 +41,67 @@ public class PlayerController : MonoBehaviour
     {
         
         float horizontal = Input.GetAxis("Horizontal");
-         if(toggle)
+        //  if(toggle)
+        // {
+        //     moveVector = new Vector3(0, 0, horizontal);
+        //     characterController.Move(moveVector*speed*Time.deltaTime);
+        // }
+
+        // else if(!toggle)
+        // {
+        //     moveVector = new Vector3(horizontal, 0, 0);
+        //     characterController.Move(moveVector*speed*Time.deltaTime);
+        //     //transform.rotation = Quaternion.AngleAxis(180,Vector3.up);
+        // }
+        //Debug.Log(horizontal);
+        if(compass == 0)
+        {
+            moveVector = new Vector3(horizontal, 0, 0);
+            characterController.Move(moveVector*speed*Time.deltaTime);
+        }
+
+        if(compass == 3 || compass == -1)
         {
             moveVector = new Vector3(0, 0, horizontal);
             characterController.Move(moveVector*speed*Time.deltaTime);
         }
 
-        else if(!toggle)
+        if(compass == 2 || compass == -2)
         {
-            moveVector = new Vector3(horizontal, 0, 0);
+            moveVector = new Vector3(-horizontal, 0, 0);
             characterController.Move(moveVector*speed*Time.deltaTime);
-            //transform.rotation = Quaternion.AngleAxis(180,Vector3.up);
         }
+
+        if(compass == 1 || compass == -3)
+        {
+            moveVector = new Vector3(0, 0, -horizontal);
+            characterController.Move(moveVector*speed*Time.deltaTime);
+        }
+
+        
+
+        
+
+        
         
     }
 
     void DirectionalRotation()
     {
         //directional rotation to left
-        if(Input.GetKeyUp(KeyCode.O))
+        if(Input.GetKeyUp(KeyCode.Q))
         {
             targetRotation *= Quaternion.AngleAxis(90,Vector3.up);
             toggle=!toggle;
+            compass += 1;
         }
 
         //directional rotation to right
-        else if(Input.GetKeyUp(KeyCode.P))
+        else if(Input.GetKeyUp(KeyCode.E))
         {
             targetRotation *= Quaternion.AngleAxis(-90,Vector3.up);
             toggle=!toggle;
+            compass -=1;
         }
     }
 
@@ -80,5 +115,13 @@ public class PlayerController : MonoBehaviour
         Vector3 v = moveVector * speed;
         v.y = gravity;
         characterController.Move(v*Time.deltaTime);
+    }
+
+    void ResetCompass()
+    {
+        if(compass == 4 || compass == -4)
+        {
+            compass = 0;
+        }
     }
 }
